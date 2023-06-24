cd52284b551da13637b403082100943193ba3de9
Bharath Kalyan <42094603+bharathkalyan00@users.noreply.github.com>
2023-06-24 23:58:15 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "af3d1b642d32d3321b118abc1a59f69a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
